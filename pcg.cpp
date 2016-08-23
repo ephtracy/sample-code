a5f2bcc4c880b9b3dac3fd3f6ef8ec7ccc393302
ephtracy <ephtracy@gmail.com>
2016-08-22 22:16:52 -0400

    pcg

---
+++ pcg.cpp
@@ -0,0 +1,28 @@
+Eigen::VectorXf PCG( const Eigen::MatrixXf &A, const Eigen::VectorXf &b, int maxIters ) {
+	Eigen::VectorXf M( A.cols() );
+	for ( int i = 0; i < A.cols(); i++ ) {
+		float a = A(i, i);
+		if ( fabs( a ) > 1e-6 ) {
+			M(i) = 1.0f / a;
+		} else {
+			M(i) = 1.0f;
+		}
+	}
+
+	Eigen::VectorXf x = Eigen::VectorXf::Zero(A.cols());
+	Eigen::VectorXf p = Eigen::VectorXf::Zero(A.cols());
+	Eigen::VectorXf r = b; // b - A * x
+
+	for ( int i = 0; i < maxIters; i++ ) {
+		Eigen::VectorXf Mr = M.array() * r.array();
+		float r_rMr = 1.0f / r.dot( Mr );
+		p += Mr * r_rMr;
+
+		Eigen::VectorXf Ap = A * p;
+		float r_pAp = 1.0f / p.dot( Ap );
+		x +=  p * r_pAp;
+		r -= Ap * r_pAp;
+	}
+
+	return x;
+}
